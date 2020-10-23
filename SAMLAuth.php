5d215dfe16ed58b7921793c7debfc26719907512
Max Wölk <max@mwoelk.de>
2020-10-23 18:50:03 +0200

    split files

--- SAMLAuth.php
+++ SAMLAuth.php
@@ -32,18 +32,47 @@ class SAMLAuthPlugin extends MantisPlugin  {
 	function hooks() {
 		$t_hooks = array(
 			'EVENT_AUTH_USER_FLAGS' => 'auth_user_flags',
+			'EVENT_LAYOUT_RESOURCES' => 'resources'
 		);
 
 		return $t_hooks;
 	}
 
+	function resources() {
+		if ( ! in_array(basename( $_SERVER['PHP_SELF'] ), ['login_page.php'] ) ) {
+			return '';
+		}
+
+		return '
+			<meta name="ssoUrl" content="' . plugin_page( 'sso', /* redirect */ true ) . '" />
+			<script type="text/javascript" src="plugins/SAMLAuth/pages/assets/js/plugin.js"></script>
+		';
+	}
+
 	function auth_user_flags( $p_event_name, $p_args ) {
 		# Don't access DB if db_is_connected() is false.
+		$t_user_id = $p_args['user_id'];
+
+		# If user is unknown, don't handle authentication for it, since this plugin doesn't do
+		# auto-provisioning
+		if( !$t_user_id ) {
+			return null;
+		}
+
+		# If anonymous user, don't handle it.
+		if( user_is_anonymous( $t_user_id ) ) {
+			return null;
+		}
+
+		$t_access_level = user_get_access_level( $t_user_id, ALL_PROJECTS );
+
+		# Have administrators use default login flow
+		if( $t_access_level >= ADMINISTRATOR ) {
+			return null;
+		}
 
 		# for everybody else use the custom authentication
 		$t_flags = new AuthFlags();
-
-		$t_flags->setLoginPage(plugin_page( 'login', /* redirect */ true ));
 
 		# Passwords managed externally for all users
 		$t_flags->setCanUseStandardLogin( false );
@@ -52,7 +81,8 @@ class SAMLAuthPlugin extends MantisPlugin  {
 		# No one can use standard auth mechanism
 
 		# Override Login page and Logout Redirect
-		$t_flags->setCredentialsPage( plugin_page( 'login', /* redirect */ true ) );
+		$t_flags->setLoginPage( plugin_page( 'sso', /* redirect */ true ) );
+		$t_flags->setCredentialsPage( plugin_page( 'sso', /* redirect */ true ) );
 		$t_flags->setLogoutRedirectPage( plugin_page( 'logout', /* redirect */ true ) );
 
 		# No long term session for identity provider to be able to kick users out.
--- pages/logout.php
+++ pages/logout.php
@@ -4,7 +4,6 @@
 
 require_once( 'core.php' );
 require_once dirname(__DIR__).'/saml/saml.php';
-require_api( 'authentication_api.php' );
 
 # User is already logged out from Mantis
 print_header_redirect( auth_login_page(), true, false );
