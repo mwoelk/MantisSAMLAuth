4c2e0507468e30a5d5511d3e7083be4a7ad059d8
Max Wölk <max@mwoelk.de>
2020-10-23 17:50:16 +0200

    changed login page

--- SAMLAuth.php
+++ SAMLAuth.php
@@ -40,30 +40,10 @@ class SAMLAuthPlugin extends MantisPlugin  {
 	function auth_user_flags( $p_event_name, $p_args ) {
 		# Don't access DB if db_is_connected() is false.
 
-		$t_username = $p_args['username'];
-
-		$t_user_id = $p_args['user_id'];
-
-		# If user is unknown, don't handle authentication for it, since this plugin doesn't do
-		# auto-provisioning
-		if( !$t_user_id ) {
-			return null;
-		}
-
-		# If anonymous user, don't handle it.
-		if( user_is_anonymous( $t_user_id ) ) {
-			return null;
-		}
-
-		$t_access_level = user_get_access_level( $t_user_id, ALL_PROJECTS );
-
-		# Have administrators use default login flow
-		if( $t_access_level >= ADMINISTRATOR ) {
-			return null;
-		}
-
 		# for everybody else use the custom authentication
 		$t_flags = new AuthFlags();
+
+		$t_flags->setLoginPage(plugin_page( 'login', /* redirect */ true ));
 
 		# Passwords managed externally for all users
 		$t_flags->setCanUseStandardLogin( false );
