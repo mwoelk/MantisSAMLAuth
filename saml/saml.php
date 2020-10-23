88e8ef3e750ea20e067a36c7c5e9344a24f0b404
Max Wölk <max@mwoelk.de>
2020-10-23 17:30:32 +0200

    fixed proxy vars

--- saml/saml.php
+++ saml/saml.php
@@ -2,5 +2,7 @@
 require_once dirname(__DIR__).'/vendor/autoload.php';
 require_once dirname(__DIR__).'/saml/settings.php';
 
+\OneLogin\Saml2\Utils::setProxyVars(true);
+
 $samlAuth = new OneLogin\Saml2\Auth($settingsInfo);
 $samlSettings = new OneLogin\Saml2\Settings($settingsInfo);
