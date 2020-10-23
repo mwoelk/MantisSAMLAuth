3c82fab1c8ffbd3721ba69f634eeac25aeb44cb2
Max Wölk <max@mwoelk.de>
2020-10-23 19:31:07 +0200

    Modified login screen

--- files/plugin.js
+++ files/plugin.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
-    //allow admin to set password directly
-    //include input for new password
-    var frm = $('#login-form');
+    const urlParams = new URLSearchParams(window.location.search);
+    const showLogin = urlParams.get('showLogin');
 
-    var ssoUrl = $("meta[name='ssoUrl']").attr('content');
-
-    $('<a id="sign_with_google" class="btn btn-primary btn-sm bigger-110" href="'+ssoUrl+'">Mit GSuite anmelden</a>').appendTo(frm);
+    if(!showLogin) {
+        var frm = $('#login-form');
+        var ssoUrl = $("meta[name='ssoUrl']").attr('content');
+        $('<a id="sign_with_google" class="btn btn-primary btn-block bigger-110" href="'+ssoUrl+'">Mit GSuite anmelden</a>').insertAfter(frm);
+        frm.remove();
+    }
 });
